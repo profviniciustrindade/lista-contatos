16140e8625a42124d679ba8a46ca277b4bc49b08
Professor Vinicius Trindade <vinicius.trindade@edu.sc.senai.br>
2026-05-11 09:56:11 -0300

    add dtos

---
+++ minha-primeira-api/src/main/java/com/senai/minha_primeira_api/dto/ContatoRequisicaoDto.java
@@ -0,0 +1,7 @@
+package com.senai.minha_primeira_api.dto;
+
+public record ContatoRequisicaoDto(
+    String nome,
+    String numero
+) {
+}
---
+++ minha-primeira-api/src/main/java/com/senai/minha_primeira_api/dto/ContatoRespostaDto.java
@@ -0,0 +1,8 @@
+package com.senai.minha_primeira_api.dto;
+
+public record ContatoRespostaDto(
+        Long id,
+        String nome,
+        String numero
+) {
+}
